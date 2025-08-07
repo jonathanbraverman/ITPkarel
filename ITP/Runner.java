44f380e7d81bfbd14593561ec48d8b9245c9cac6
Jonathan Braverman <jonathan.braverman@ctstate.edu>
2025-08-07 16:24:49 -0400

    goal1

--- ITP/Runner.java
+++ ITP/Runner.java
@@ -12,9 +12,9 @@ public class Runner
       System.out.println("Starting...");
       World.repaint();
       //World.setSize(10,10);
-      //World.setDelay(50);
+      World.setDelay(10);
       World.readWorld("worlds", "firstworld.kwld");
-      myrobot = new ITPRobot(1,1,Directions.North,1);
+      myrobot = new ITPRobot(1,1,Directions.North,0);
       //builder = new WorldBuilder(true);
       World.setVisible(true);
       
@@ -25,7 +25,34 @@ public class Runner
       myrobot.move();
       myrobot.move();
       myrobot.move();
-      
+      myrobot.move();
+      myrobot.move();
+      myrobot.move();
+      myrobot.move();
+      myrobot.move();
+      myrobot.turnLeft();
+      myrobot.move();
+      myrobot.move();
+      myrobot.move();
+      myrobot.turnLeft();
+      myrobot.turnLeft();
+      myrobot.turnLeft();
+      myrobot.move();
+      myrobot.turnLeft();
+      myrobot.turnLeft();
+      myrobot.turnLeft();
+      myrobot.move();
+      myrobot.move();
+      myrobot.move();
+      myrobot.pickBeeper();
+      myrobot.turnLeft();
+      myrobot.turnLeft();
+      myrobot.move();
+      myrobot.move();
+      myrobot.move();
+      myrobot.putBeeper();
+      myrobot.turnLeft();
+      myrobot.move();
   }
     
   public static void main(String[] args)
